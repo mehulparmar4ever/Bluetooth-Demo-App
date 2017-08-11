3fb3234a19c1af45dd5e16d4ef66c55f7ce453f7
Pavel Kazantsev <pv.kazantsev@gmail.com>
2017-08-11 22:54:37 +0700

    Initial commit.

---
+++ Sender/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Sender
+//
+//  Created by Pavel Kazantsev on 8/11/17.
+//  Copyright © 2017 PaKaz.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
